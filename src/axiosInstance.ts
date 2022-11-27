3d9d19744209aee7a083b6eaa5d50271bd10613e
vvviiitttaaalll123 <ummibaby114@gmail.com>
2022-11-27 12:22:09 +0530

    added content type

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -3,5 +3,7 @@ import axios from "axios";
 export const instance = axios.create({
   headers: {
     accept: "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
   },
 });
