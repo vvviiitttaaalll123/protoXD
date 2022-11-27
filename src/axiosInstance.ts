679fe8f95d31079342e29980e07150d171a73613
vvviiitttaaalll123 <ummibaby114@gmail.com>
2022-11-27 19:54:05 +0530

    added netlify cli

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -2,9 +2,8 @@ import axios from "axios";
 
 export const instance = axios.create({
   headers: {
-    accept: "application/json",
+    'accept': 'application/json',
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
-  },
-  withCredentials: false,
+  }
 });
