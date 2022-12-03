fad5e3fd8a1f5699e4628cabab9352c069d2ed56
vvviiitttaaalll123 <vitalblaze2@gmail.com>
2022-12-03 13:36:58 +0530

    added trailing /

--- src/constants/AllUrls.ts
+++ src/constants/AllUrls.ts
@@ -5,29 +5,29 @@ export const GET_SEASONAL_UPDATES =
 
 export const GET_RECENTLY_UPDATED =
   BASE_URL +
-  "get_recently_updated";
+  "get_recently_updated/";
 
 export const GET_RECENTLY_ADDED =
   BASE_URL +
-  "get_recently_added";
+  "get_recently_added/";
 
-export const COVER_URL = BASE_URL + "get_cover";
+export const COVER_URL = BASE_URL + "get_cover/";
 
 export const MANGA_DETAILS_URL_BUILD =
   BASE_URL +
-  "get_manga_details";
+  "get_manga_details/";
 
-export const IMAGE_URL = BASE_URL + "get_image_url"
+export const IMAGE_URL = BASE_URL + "get_image_url/"
 
 export const PERTICULAR_MANGA = BASE_URL + "manga/"
 
-export const GET_CHAPTERS = BASE_URL + "get_chapters"
+export const GET_CHAPTERS = BASE_URL + "get_chapters/"
 
-export const GET_CHAPTER_IMAGES = BASE_URL + "get_chapter_images"
+export const GET_CHAPTER_IMAGES = BASE_URL + "get_chapter_images/"
 
-export const GET_CHAPTER_DETAIL = BASE_URL + "get_chapter_detail"
+export const GET_CHAPTER_DETAIL = BASE_URL + "get_chapter_detail/"
 
-export const SEARCH_URL = BASE_URL + "search"
+export const SEARCH_URL = BASE_URL + "search/"
 
 
 // {ids:[ 
